86a965248fb502a30fe5f69c53ae8ef445e133d8
blen36 <bankoegor@gmail.com>
2025-09-25 01:23:29 +0300

    Задание 1 выполнено

--- lab_2/lab_2_1/task_1.py
+++ lab_2/lab_2_1/task_1.py
@@ -1,2 +1,15 @@
-lst = [1,2,3,4,5]
-print(lst)
+inputStr=input("Введите текст: ").split()
+wordCount={}
+for word in inputStr:
+    wordLower=word.lower()
+    if wordLower in wordCount:
+        wordCount[wordLower]+=1
+    else:
+        wordCount[wordLower]=1
+print(f"Словарь будет таким: {wordCount}")
+
+uniqueWordCount=0
+for word in wordCount:
+    if wordCount[word] == 1:
+        uniqueWordCount += 1
+print(f"Количество уникальных слов: {uniqueWordCount}")
