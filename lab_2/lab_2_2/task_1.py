a9197f43dfe0012799dd6868cd63e8a3e4f98b2d
blen36 <bankoegor@gmail.com>
2025-09-28 15:53:46 +0300

    добавил task_1

--- lab_2/lab_2_2/task_1.py
+++ lab_2/lab_2_2/task_1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+def flatten(lst):
+    i = 0
+    while i < len(lst):
+        if isinstance(lst[i], list):
+            nested_list=lst[i]
+            lst[i: i+1]=nested_list
+        else:
+            i += 1
+    if any(isinstance(i, list) for i in lst):
+        flatten(lst)
+lst=[[1,2,3],[4,5,6],[7,8,9]]
+print(f"Лист до: {lst}")
+flatten(lst)
+print(f"Лист после: {lst}")
