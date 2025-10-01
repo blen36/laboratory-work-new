f153355600eb08b079fd8a2af4a6e9ca3b11d2f9
blen36 <bankoegor@gmail.com>
2025-10-01 16:19:04 +0300

    add comments

--- lab_1/task_7.py
+++ lab_1/task_7.py
@@ -2,12 +2,12 @@ second=int(input("Введите количество секунд: "))
 minute=second//60
 second_remainder=second%60
 
-if minute % 10 == 1 and minute % 100 != 11:
+if minute % 10 == 1 and minute % 100 != 11: # 1, 21, 31 минута (но не 11)
     minute_str = "минута"
 elif 2 <= minute % 10 <= 4 and (minute % 100 < 10 or minute % 100 >= 20):
-    minute_str = "минуты"
+    minute_str = "минуты"  # 2-4, 22-24, 32-34 минуты
 else:
-    minute_str = "минут"
+    minute_str = "минут"  # 0, 5-20, 25-30, 35-40 минут
 
 if second_remainder % 10 == 1 and second_remainder % 100 != 11:
     second_str = "секунда"
