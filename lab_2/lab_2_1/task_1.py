ffe6a4dd1fec976bd3ff4f1df50183476d2a23fe
blen36 <bankoegor@gmail.com>
2025-10-29 15:38:45 +0300

    add correct task_1

--- lab_2/lab_2_1/task_1.py
+++ lab_2/lab_2_1/task_1.py
@@ -1,15 +1,10 @@
-inputStr=input("Введите текст: ").split()
-wordCount={}
-for word in inputStr:
-    wordLower=word.lower()
-    if wordLower in wordCount:
-        wordCount[wordLower]+=1
+str = input("Введите строку: ").lower().split()
+d = {}
+for word in str:
+    if word in d:
+        d[word] += 1
     else:
-        wordCount[wordLower]=1
-print(f"Словарь будет таким: {wordCount}")
+        d[word] = 1
 
-uniqueWordCount=0
-for word in wordCount:
-    if wordCount[word] == 1:
-        uniqueWordCount += 1
-print(f"Количество уникальных слов: {uniqueWordCount}")
+print(d)
+print(len(set(str)))
