8f99503d524f0a7f4c43e2d5dd08d56a0cc139bc
blen36 <bankoegor@gmail.com>
2025-10-29 15:36:54 +0300

    resolve task_1

--- lab_2/lab_2_1/task_1_1.py
+++ lab_2/lab_2_1/task_1_1.py
@@ -7,9 +7,4 @@ for word in str:
         d[word] = 1
 
 print(d)
-uniqueWordCount=0
-for word in d:
-    if d[word] == 1:
-        uniqueWordCount += 1
-print(f"Количество уникальных слов: {uniqueWordCount}")
-#или так uniqueWordCount = sum(1 for count in d.values() if count == 1)
+print(len(set(str)))
