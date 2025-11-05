4f428ef277408f10c81b3fb28058da201436c754
blen36 <bankoegor@gmail.com>
2025-11-05 22:36:01 +0300

    cleanup: удалить папку lab_1

--- lab_1/task_6.py
+++
@@ -1,7 +0,0 @@
-pressure = float(input("Введите давление в Паскалях: "))
-volume = float(input("Введите объем в м^3: "))
-temperature =float(input("Введите температуру в Кельвинах: "))
-
-R=8.31
-n=(pressure*volume)/(R*temperature)
-print(f"Количество газа в молях: {n:.2f}")
